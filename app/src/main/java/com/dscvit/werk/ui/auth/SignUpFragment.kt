42f4fc59af9441cd1a5972b869c8c375f05a1ad7
Rithik Jain <rithik.jain3006@gmail.com>
2021-04-27 01:10:50 +0530

    feat: add shared axis transition for logo

--- app/src/main/java/com/dscvit/werk/ui/auth/SignUpFragment.kt
+++ app/src/main/java/com/dscvit/werk/ui/auth/SignUpFragment.kt
@@ -7,8 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dscvit.werk.R
+import com.dscvit.werk.databinding.FragmentSignUpBinding
 
-class SignInFragment : Fragment() {
+class SignUpFragment : Fragment() {
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +25,8 @@ class SignInFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    ): View {
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
--- app/src/main/java/com/dscvit/werk/ui/auth/WelcomeFragment.kt
+++ app/src/main/java/com/dscvit/werk/ui/auth/WelcomeFragment.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
+import com.dscvit.werk.R
 import com.dscvit.werk.databinding.FragmentWelcomeBinding
 
 
 class WelcomeFragment : Fragment() {
-    private var _binding: FragmentWelcomeBinding ?= null
+    private var _binding: FragmentWelcomeBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +27,22 @@ class WelcomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.accountExistsText.setOnClickListener {
-            val action = WelcomeFragmentDirections.actionWelcomeFragmentToSignInFragment()
-            findNavController().navigate(action)
+            val extras = FragmentNavigatorExtras(binding.logo to "logo_small")
+            findNavController().navigate(
+                R.id.action_welcomeFragment_to_signInFragment, null,
+                null,
+                extras
+            )
+        }
+
+        binding.emailSignUpButton.setOnClickListener {
+            val extras = FragmentNavigatorExtras(binding.logo to "logo_small")
+            findNavController().navigate(
+                R.id.action_welcomeFragment_to_signUpFragment,
+                null,
+                null,
+                extras
+            )
         }
     }
 }

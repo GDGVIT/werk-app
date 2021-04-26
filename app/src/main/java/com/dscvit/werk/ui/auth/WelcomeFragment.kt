89f951ca8cee7ceb381e67123bb2509a42730ef1
Rithik Jain <rithik.jain3006@gmail.com>
2021-04-27 00:20:31 +0530

    feat: sign in page ui complete

--- app/src/main/java/com/dscvit/werk/ui/auth/WelcomeFragment.kt
+++ app/src/main/java/com/dscvit/werk/ui/auth/WelcomeFragment.kt
@@ -5,14 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.dscvit.werk.R
+import androidx.navigation.fragment.findNavController
+import com.dscvit.werk.databinding.FragmentWelcomeBinding
 
 
 class WelcomeFragment : Fragment() {
+    private var _binding: FragmentWelcomeBinding ?= null
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.accountExistsText.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToSignInFragment()
+            findNavController().navigate(action)
+        }
     }
 }

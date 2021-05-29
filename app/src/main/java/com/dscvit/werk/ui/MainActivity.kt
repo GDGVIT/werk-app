697b01b94fd80e6b18c40f271b620ecda382a57b
Rithik Jain <rithik.jain3006@gmail.com>
2021-05-30 02:44:57 +0530

    feat: setup logic for checking if user is authenticated or not

--- app/src/main/java/com/dscvit/werk/ui/MainActivity.kt
+++ app/src/main/java/com/dscvit/werk/ui/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.dscvit.werk.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.dscvit.werk.R
+import com.dscvit.werk.util.APP_PREF
+import com.dscvit.werk.util.PREF_TOKEN
+import com.dscvit.werk.util.PrefHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +18,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPrefs = PrefHelper.customPrefs(this, APP_PREF)
+        val jwtToken = sharedPrefs.getString(PREF_TOKEN, "")
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val graph = navController.graph
+        graph.startDestination =
+            if (jwtToken == "") R.id.welcomeFragment else R.id.sessionsOverviewFragment
+        navController.graph = graph
     }
 }

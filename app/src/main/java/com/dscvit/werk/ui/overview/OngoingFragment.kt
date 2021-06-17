73e5c2cb3e42b4b0b0c680653ac335520833ec43
Rithik Jain <rithik.jain3006@gmail.com>
2021-06-17 13:43:37 +0530

    feat: minor ui changes and navigate from session overview ongoing card to session activity

--- app/src/main/java/com/dscvit/werk/ui/overview/OngoingFragment.kt
+++ app/src/main/java/com/dscvit/werk/ui/overview/OngoingFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dscvit.werk.databinding.FragmentOngoingBinding
 import com.dscvit.werk.ui.adapter.OngoingSessionsAdapter
+import com.dscvit.werk.ui.utils.OnItemClickListener
+import com.dscvit.werk.ui.utils.addOnItemClickListener
 
 class OngoingFragment : Fragment() {
     private var _binding: FragmentOngoingBinding? = null
@@ -28,5 +31,13 @@ class OngoingFragment : Fragment() {
         binding.recyclerView.adapter = OngoingSessionsAdapter()
         binding.recyclerView.layoutManager =
             LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        binding.recyclerView.addOnItemClickListener(object : OnItemClickListener {
+            override fun onItemClicked(position: Int, view: View) {
+                val action =
+                    SessionsOverviewFragmentDirections.actionSessionsOverviewFragmentToSessionActivity()
+                findNavController().navigate(action)
+            }
+        })
     }
 }

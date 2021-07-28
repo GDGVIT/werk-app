20381b502d54a8c61f0accee64ce48e4d889e02c
Rithik Jain <rithik.jain3006@gmail.com>
2021-07-29 02:14:32 +0530

    feat: core google sign in added

--- app/src/main/java/com/dscvit/werk/ui/auth/WelcomeFragment.kt
+++ app/src/main/java/com/dscvit/werk/ui/auth/WelcomeFragment.kt
@@ -1,19 +1,41 @@
 package com.dscvit.werk.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import com.dscvit.werk.R
 import com.dscvit.werk.databinding.FragmentWelcomeBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class WelcomeFragment : Fragment() {
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
+
+    private val TAG: String = this.javaClass.simpleName
+
     private var _binding: FragmentWelcomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +47,18 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+        auth = Firebase.auth
+
+        binding.googleSignUpButton.setOnClickListener {
+            signIn()
+        }
 
         binding.accountExistsText.setOnClickListener {
             val extras = FragmentNavigatorExtras(binding.logo to "logo_small")
@@ -44,5 +78,61 @@ class WelcomeFragment : Fragment() {
                 extras
             )
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN && data != null) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            var idToken: String
+            val googleToken = account?.idToken
+            Log.d(TAG, googleToken ?: "")
+            val credential = GoogleAuthProvider.getCredential(googleToken, null)
+            auth.signInWithCredential(credential)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success")
+                        val user = auth.currentUser
+                        user?.getIdToken(true)?.addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                idToken = it.result?.token ?: ""
+                                Log.d(TAG, idToken)
+                                // Send token to your backend
+                            } else {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Some error occurred",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    }
+                }
+            // Signed in successfully, show authenticated UI.
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+        }
+    }
+
+    private fun signIn() {
+        googleSignInClient.signOut()
+        auth.signOut()
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 }
